e563779e42df01ad595ef2a7abc2c8605f5dc7a4
Myo Myint Aung <myomyintaung3535@gmail.com>
2020-06-19 16:15:50 +0630

    main view fxml

---
+++ student-management/src/com/main/StudentManagementApp.java
@@ -0,0 +1,25 @@
+package com.main;
+
+import com.main.view.MainView;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StudentManagementApp extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(MainView.class.getResource("MainView.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+}
---
+++ student-management/src/com/main/view/MainView.java
@@ -0,0 +1,5 @@
+package com.main.view;
+
+public class MainView {
+
+}
